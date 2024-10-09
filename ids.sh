5318b53181e5237240fb2b76de1a11594b80d947
Vladan Kudlac <vladan.kudlac@maptiler.com>
2024-10-09 20:17:33 +0200

    Use bash set builtin instead of chaining commands

--- ids.sh
+++ ids.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-curl 'https://overpass-api.de/api/interpreter?data=%5Bout%3Axml%5D%3B%0Aarea%5Bname%3D%22%C4%8Cesko%22%5D-%3E.a%3B%0A%28%0A%09rel%28area.a%29%5Broute%3Dtram%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dbus%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dtrain%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Drailway%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dsubway%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dtrolleybus%5D%3B%0A%29%3B%0Aout%20meta%3B' -o ids.osm \
-&& echo "xsltproc started" \
-&& xsltproc --stringparam current-date `date +%d.%m.%Y` -o output.txt ids.xsl ids.osm \
-&& echo "analysis started" \
-&& awk -f ids.awk output.txt > outputAnalyzed.txt \
-&& rm temp.html
+set -e
+
+curl 'https://overpass-api.de/api/interpreter?data=%5Bout%3Axml%5D%3B%0Aarea%5Bname%3D%22%C4%8Cesko%22%5D-%3E.a%3B%0A%28%0A%09rel%28area.a%29%5Broute%3Dtram%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dbus%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dtrain%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Drailway%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dsubway%5D%3B%0A%20%20%09rel%28area.a%29%5Broute%3Dtrolleybus%5D%3B%0A%29%3B%0Aout%20meta%3B' -o ids.osm
+echo "xsltproc started"
+xsltproc --stringparam current-date `date +%d.%m.%Y` -o output.txt ids.xsl ids.osm
+echo "analysis started"
+awk -f ids.awk output.txt > outputAnalyzed.txt
+rm temp.html
